10ef9df2c57cde7ec26f8fc1e3ec665cd4c3b481
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-03-07 15:58:15 -0600

    added test for testing that the route handlers will return the correct information

--- api/server.spec.js
+++ api/server.spec.js
@@ -22,4 +22,21 @@ describe('server.js', () => {
             expect(res.body).toEqual({ api: 'running' });
         });
     });
+
+    describe('POST /posts', () => {
+        it('should return a status code of 200 OK', async () => {
+            const post = {title: 'yo', content: 'Something'};
+            const res = await request(server).post('/posts', post);
+
+            expect(res.status).toBe(200);
+        });
+
+        it('should return a JSON object of the post added', async () => {
+            const post = {title: 'yo', content: 'Something'};
+            const res = await request(server).post('/posts', post);
+
+            expect(res.type).toMatch(/json/i);
+            expect(res.body).toEqual({title: 'yo', content: 'Something'});
+        });
+    })
 });
